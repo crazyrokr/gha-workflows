88fcb80e87c8117d5d5f885b42051b1a578a7d8c
Maksim Kashapov <56276969+crazyrokr@users.noreply.github.com>
2026-06-01 12:14:02 +0200

    Fix search pattern

--- ha-addon-auto-locker/lock.py
+++ ha-addon-auto-locker/lock.py
@@ -49,7 +49,7 @@ def lock_build_yaml(file_path, lock_data):
 
     # Regex to find architecture-specific images in build.yaml
     # Matches:   amd64: "image:tag"
-    pattern = re.compile(r'(\s+)(\w+):\s+"([^"@]+)(?::([^"@]+))?(@sha256:[a-f0-9]+)?"')
+    pattern = re.compile(r'(\s*)(\w+):*\s+"*([^"@]+)(?::([^"@]+))?(@sha256:[a-f0-9]+)?"*')
     
     new_content = content
     changes_made = False
